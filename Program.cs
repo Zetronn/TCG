f4c676ebafcebe33b820f162f99ab8e98788b23e
Phantom <147918593+Zetronn@users.noreply.github.com>
2026-01-11 22:54:54 +0100

    Fixed the Main method and updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using TCGMain;
+﻿using System;
+using TCGMain;
 
+namespace TCGMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cards Bulbazaur = new Cards();
+            Bulbazaur.Name = "Bulbazaur";
+            Bulbazaur.Health = 100;
 
+            Bulbazaur.showHP();
 
-
-static void Main(string[] args){
-    Cards Bulbazaur = new Cards();
-    Bulbazaur.Name = "Bulbazaur";
-    Bulbazaur.Health = 100;
-    Bulbazaur.showHP();
-    Console.ReadKey();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
 }
