a21a326890e9bbe427da97b2083bd3e2956997bd
Phantom <147918593+Zetronn@users.noreply.github.com>
2026-01-11 22:26:10 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using TCGMain;
+
+
+
+
+static void Main(string[] args){
+    Cards Bulbazaur = new Cards();
+    Bulbazaur.Name = "Bulbazaur";
+    Bulbazaur.Health = 100;
+    Bulbazaur.showHP();
+    Console.ReadKey();
+}
